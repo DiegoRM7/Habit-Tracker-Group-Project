e8421570f55bc34bba861258e476137ca4b02975
darius89tech <dariusqw89@gmail.com>
2023-12-14 21:53:08 -0500

    changes to the routes and controllers remaining added

--- flask_app/users.py
+++ flask_app/users.py
@@ -11,3 +11,28 @@ def register():
 @app.get("/login")
 def login():
     return "login template"
+
+
+@app.get("/welcome")
+def login():
+    return "welcome template"
+
+
+@app.get("/habit details")
+def login():
+    return "details template"
+
+
+@app.get("/update")
+def login():
+    return "update template"
+
+
+@app.get("/account details")
+def login():
+    return "account details template"
+
+
+@app.get("/create habit")
+def login():
+    return "create habit template"
