4bd752252fda669dfef8509b5e0d8f361aa484ed
darius89tech <dariusqw89@gmail.com>
2023-12-16 01:54:12 -0500

    port number changed

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask_app import app
-from flask_app.controllers import users #controllers go here, if you add a new controller file you have to also add it here
+from flask_app.controllers import (
+    users,
+)  # controllers go here, if you add a new controller file you have to also add it here
+
 # This is where Flask's thought process starts. The request enters here, then goes to the controller above which has a matching route.
 
-if __name__=="__main__":   
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=5001)
     # On line six you can change the port number.
 
 # debug needs to be set to False when deployed.
