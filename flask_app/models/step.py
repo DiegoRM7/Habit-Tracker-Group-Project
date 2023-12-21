01256f42bb986b12aee2b3b2fc56c1027012306f
darius89tech <dariusqw89@gmail.com>
2023-12-21 00:48:11 -0500

    waiting mysql script

--- flask_app/models/step.py
+++ flask_app/models/step.py
@@ -1,44 +1,46 @@
-
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash, session
 import re
 from flask_bcrypt import Bcrypt
+
 bcrypt = Bcrypt(app)
 # The above is used when we do login registration, flask-bcrypt should already be in your env check the pipfile
 
 # Remember 'fat models, skinny controllers' more logic should go in here rather than in your controller. Your controller should be able to just call a function from the model for what it needs, ideally.
 
+
 class Step:
     db = "habit_tracker_schema"
-    def __init__(self, data):
-        self.step_id = data['step_id']
-        self.first_name = data['amount']
-        self.last_name = data['location']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
-        self.user_id = data['user_id']
 
-#?? Create Step Models
+    def __init__(self, data):
+        self.step_id = data["step_id"]
+        self.amount = data["amount"]
+        self.location = data["location"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        self.user_id = data["user_id"]
+
+    # ?? Create Step Models
     @classmethod
-    def create_step_habit(cls,step_data):
+    def create_step_habit(cls, step_data):
         query = """
                 INSERT INTO steps (amount, location, user_id)
                 VALUES (%(amount)s, %(location)s, %(user_id)s)
                 ;"""
-        step_id = connectToMySQL(cls.db).query_db(query,step_data)
+        step_id = connectToMySQL(cls.db).query_db(query, step_data)
         return step_id
 
-#?? Read Step Models
+    # ?? Read Step Models
 
     @classmethod
-    def get_step_habit_by_habit_id(cls,step_id):
+    def get_step_habit_by_habit_id(cls, step_id):
         query = """
                 SELECT *
                 FROM steps 
                 WHERE step_id = %(step_id)s
                 ;"""
-        results = connectToMySQL(cls.db).query_db(query, {"step_id" : step_id})
+        results = connectToMySQL(cls.db).query_db(query, {"step_id": step_id})
         return cls(results)
 
     @classmethod
@@ -52,19 +54,19 @@ class Step:
         for a_step_habit in results:
             step_habit.append(cls(a_step_habit))
         return step_habit
-    
+
     @classmethod
-    def get_all_step_habits_by_user_id(cls,user_id):
+    def get_all_step_habits_by_user_id(cls, user_id):
         query = """
                 SELECT * FROM steps
                 JOIN user
                 ON user.id = step.user_id
                 WHERE step.user_id = %(user_id);
                 """
-        results = connectToMySQL(cls.db).query_db(query,{"user_id": user_id})
+        results = connectToMySQL(cls.db).query_db(query, {"user_id": user_id})
         return results
-    
-#?? Update Step Models
+
+    # ?? Update Step Models
     @classmethod
     def update_steps(cls, data):
         query = """
@@ -74,27 +76,30 @@ class Step:
                 location = %(location)s,
                 WHERE steps_id = %(steps_id)s;
                 """
-        return connectToMySQL(cls.db).query_db(query,data)
+        return connectToMySQL(cls.db).query_db(query, data)
         # ! will eventually return True for validation purposes
 
-#?? Delete Step Models
+    # ?? Delete Step Models
     @classmethod
-    def delete_steps(cls,steps_id):
+    def delete_steps(cls, steps_id):
         query = """
                 DELETE FROM steps
                 WHERE steps_id = %(steps_id)s;
                 """
-        return connectToMySQL(cls.db).query_db(query, {'steps_id' : steps_id})
-    
-#?? Step Validation
+        return connectToMySQL(cls.db).query_db(query, {"steps_id": steps_id})
+
+    # ?? Step Validation
 
     @staticmethod
     def validate_step_habits(data):
         is_valid = True
-        if len(data['amount']) < 1:
-            flash("Please enter a step amount greater than 1, lets go knees to chest!!!","register_validation")
+        if len(data["amount"]) < 1:
+            flash(
+                "Please enter a step amount greater than 1, lets go knees to chest!!!",
+                "step_validation",
+            )
             is_valid = False
-        if len(data['location']) < 1 :
-            flash("Please enter the location your steps occured","register_validation")
+        if len(data["location"]) < 1:
+            flash("Please enter the location your steps occured", "step_validation")
             is_valid = False
         return is_valid
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,45 +1,48 @@
-
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash, session
 import re
 from flask_bcrypt import Bcrypt
+
 bcrypt = Bcrypt(app)
 # The above is used when we do login registration, flask-bcrypt should already be in your env check the pipfile
 
 # Remember 'fat models, skinny controllers' more logic should go in here rather than in your controller. Your controller should be able to just call a function from the model for what it needs, ideally.
 
+
 class User:
     db = "habit_tracker_schema"
+
     def __init__(self, data):
-        self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.username = data['username']
-        self.location = data['location']
-        self.email = data['email']
-        self.password = data['password']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
-        self.gym = []   # holds all the gym habit tracked sessions for this user
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.username = data["username"]
+        self.location = data["location"]
+        self.email = data["email"]
+        self.password = data["password"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        self.gym = []  # holds all the gym habit tracked sessions for this user
         self.sleep = []  # sleep habit tracked sessions
         self.step = []  # step habit tracked sessions
 
     # Create Users Models
     @classmethod
-    def create_user(cls,user_data):
-        if not cls.validate_user(user_data):
-            return False
+    def create_user(cls, user_data):
+        """this method does not exist"""
+        # if not cls.validate_user(user_data):
+        #     return False
         user_data = user_data.copy()
-        user_data['password'] = bcrypt.generate_password_hash(user_data['password'])
+        user_data["password"] = bcrypt.generate_password_hash(user_data["password"])
         query = """
                 INSERT INTO user (first_name, last_name, username, location, email, password)
                 VALUES (%(first_name)s, %(last_name)s, %(username)s, %(location)s, %(email)s, %(password)s)
                 ;"""
         user_id = connectToMySQL(cls.db).query_db(query, user_data)
-        session['user_id'] = user_id 
-        session['first_name'] = user_data['first_name']
-        #When user successfully logs in first_name and user_id are put into session for security and UI purposes.
+        session["user_id"] = user_id
+        session["first_name"] = user_data["first_name"]
+        # When user successfully logs in first_name and user_id are put into session for security and UI purposes.
         return user_id
 
     # Read Users Models
@@ -51,13 +54,13 @@ class User:
                 FROM user
                 WHERE email = %(email)s;
                 """
-        result = connectToMySQL(cls.db).query_db(query, {'email': email})
+        result = connectToMySQL(cls.db).query_db(query, {"email": email})
         if result:
             this_user = cls(result[0])
             return this_user
         return False
-    
-# <<<<<<< DiegoM
+
+    # <<<<<<< DiegoM
     @classmethod
     def get_user_by_user_id_logged_in(cls, user_id):
         query = """
@@ -65,62 +68,59 @@ class User:
                 FROM user
                 WHERE id = %(user_id)s;
                 """
-        result = connectToMySQL(cls.db).query_db(query, {'user_id': user_id})
-        print("_____________________",result)
+        result = connectToMySQL(cls.db).query_db(query, {"user_id": user_id})
+        print("_____________________", result)
         return result
-    
+
     # Update Users Models
-# =======
-#?? Update Users Models
-# >>>>>>> main
+    # =======
+    # ?? Update Users Models
+    # >>>>>>> main
 
+    # ?? Delete Users Models
 
-#?? Delete Users Models
-
-
-
-#?? user_login
+    # ?? user_login
 
     @classmethod
     def login(cls, data):
         # ? Diego: instantiated object once, then again in login method when it's being called on. fixed, test if works after.
-        this_user = User.get_user_by_email(data['email'])
+        this_user = User.get_user_by_email(data["email"])
         if this_user:
-            if bcrypt.check_password_hash(this_user.password, data['password']):
-                session['user_id'] = this_user.id
-                session['first_name'] = this_user.first_name
+            if bcrypt.check_password_hash(this_user.password, data["password"]):
+                session["user_id"] = this_user.id
+                session["first_name"] = this_user.first_name
                 # could also save username into session here as well
                 return True
             # edited to make flash show when specifically called on login page
-        flash("Invalid Login Information",'no_user_shown_in_DB')
+        flash("Invalid Login Information", "no_user_shown_in_DB")
         return False
 
-    #user_validation
+    # user_validation
     @staticmethod
     def validate_user_registration(data):
-        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
         is_valid = True
-        if len(data['email']) < 1:
-            flash("Email required","register_validation")
+        if len(data["email"]) < 1:
+            flash("Email required", "register_validation")
             is_valid = False
-        if not EMAIL_REGEX.match(data['email']):
-            flash("Invalid email","register_validation")
+        if not EMAIL_REGEX.match(data["email"]):
+            flash("Invalid email", "register_validation")
             is_valid = False
-        if User.get_user_by_email(data['email']):
-            flash("Email taken","register_validation")
-        if len(data['location']) < 2:
-            flash("Please provide Location","register_validation")
+        if User.get_user_by_email(data["email"]):
+            flash("Email taken", "register_validation")
+        if len(data["location"]) < 2:
+            flash("Please provide Location", "register_validation")
             is_valid = False
-        if len(data['first_name']) < 2:
-            flash("First Name must be at least 2 characters","register_validation")
+        if len(data["first_name"]) < 2:
+            flash("First Name must be at least 2 characters", "register_validation")
             is_valid = False
-        if len(data['last_name']) < 2:
-            flash("Last Name be at least 2 characters","register_validation")
+        if len(data["last_name"]) < 2:
+            flash("Last Name be at least 2 characters", "register_validation")
             is_valid = False
-        if len(data['password']) < 8:
-            flash("Password must be at least 8 characters","register_validation")
+        if len(data["password"]) < 8:
+            flash("Password must be at least 8 characters", "register_validation")
             is_valid = False
-        if data['password'] != data['confirm_password']:
-            flash("Passwords must match","register_validation")
+        if data["password"] != data["confirm_password"]:
+            flash("Passwords must match", "register_validation")
             is_valid = False
         return is_valid
